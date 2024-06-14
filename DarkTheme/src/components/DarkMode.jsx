fd4b05bd986eecd0d4aaa6297f97a132d5279479
Manthan Thakor <thakormanthan849@gmail.com>
2024-06-14 15:58:34 +0530

    React Dark theme completed

--- DarkTheme/src/components/DarkMode.jsx
+++ DarkTheme/src/components/DarkMode.jsx
@@ -4,18 +4,33 @@ import SunIcon from "./SunIcon";
 import MoonIcon from "./MoonIcon";
 
 const DarkMode = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("selectedTheme") || "light");
+
   const setDarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
+    localStorage.setItem("selectedTheme", "dark");
+    setTheme("dark");
   };
 
   const setLightMode = () => {
     document.querySelector("body").setAttribute("data-theme", "light");
+    localStorage.setItem("selectedTheme", "light");
+    setTheme("light");
   };
 
   const toggleTheme = (e) => {
     if (e.target.checked) setDarkMode();
     else setLightMode();
   };
+
+  useEffect(() => {
+    const selectedTheme = localStorage.getItem("selectedTheme");
+    if (selectedTheme === "dark") {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
+  }, []);
 
   return (
     <div className='dark_mode'>
@@ -24,6 +39,7 @@ const DarkMode = () => {
         type='checkbox'
         id='darkmode-toggle'
         onChange={toggleTheme}
+        checked={theme === "dark"}
       />
       <label className='dark_mode_label' htmlFor='darkmode-toggle'>
         <SunIcon />
